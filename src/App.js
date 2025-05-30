c9a4e068a8fc25da61c76c6d7ab8710d5e41859b
103806340 <anhnd201203@gmail.com>
2025-05-30 15:01:18 +1000

    updated code structure by moving pages to components directory

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './LandingPage';
+import LandingPage from './components/landingpage/LandingPage';
 import ClientHomePage from './components/ClientHomePage';
 import SensorProviderHomePage from './components/SensorProviderHomePage';
 import BlogPage from './components/blog/blog';
 import HelpPage from './components/help/help';
 import MarketPage from './components/market/market';
 import SelldataPage from './components/selldata/selldata';
-import SignInPage from './SignInPage';        
-import RegisterPage from './RegisterPage';       
-import PreferencesPage from './PreferencesPage';  
+import SignInPage from './components/signin/SignInPage';        
+import RegisterPage from './components/register/RegisterPage';       
+import PreferencesPage from './components/preferencepage/PreferencesPage';  
 import BuyPage from './components/buysensors/BuyPage'; 
 import SensorDataPage from './components/data/SensorDataPage';
 import Cart from './components/cart/Cart';
 import PurchaseHistory from './components/purchasehistory/purchasehistory';
-import ProfilePage from './profile/ProfilePage';
+import ProfilePage from './components/profile/ProfilePage';
 import './App.css';
 
 function App() {
